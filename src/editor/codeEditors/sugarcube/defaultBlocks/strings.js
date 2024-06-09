9a2e6544dea26915d7b837d0706d8abe9c8ac81a
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-06-08 20:25:59 -0400

    String Blocks

--- src/editor/codeEditors/sugarcube/defaultBlocks/strings.js
+++ src/editor/codeEditors/sugarcube/defaultBlocks/strings.js
@@ -17,6 +17,7 @@
             arguments: {
               A: {
                 type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
               },
             },
           },
@@ -27,22 +28,232 @@
             arguments: {
               A: {
                 type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
               },
               B: {
                 type: sugarcube.ArgumentType.STRING,
+                defaultValue:"banana"
+              },
+            },
+          },
+          "---",
+          {
+            opcode: "reverse",
+            type: sugarcube.BlockType.REPORTER,
+            text: "reverse [A]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              }
+            },
+          },
+          {
+            opcode: "repeat",
+            type: sugarcube.BlockType.REPORTER,
+            text: "repeat [A] [B] times",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              },
+              B: {
+                type: sugarcube.ArgumentType.NUMBER,
+                defaultValue:1
+              },
+            },
+          },
+          {
+            opcode: "replace",
+            type: sugarcube.BlockType.REPORTER,
+            text: "replace [A] in [B] with [C]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              },
+              B: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple banana"
+              },
+              C: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"pear"
+              },
+            },
+          },
+          "---",
+          {
+            opcode: "characterAt",
+            type: sugarcube.BlockType.REPORTER,
+            text: "letter [A] of [B]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.NUMBER,
+                defaultValue:1
+              },
+              B: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              },
+            },
+          },
+          {
+            opcode: "charactersBetween",
+            type: sugarcube.BlockType.REPORTER,
+            text: "letters [A] to [B] of [C]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.NUMBER,
+                defaultValue:1
+              },
+              B: {
+                type: sugarcube.ArgumentType.NUMBER,
+                defaultValue:3
+              },
+              C: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              },
+            },
+          },
+          {
+            opcode: "split",
+            type: sugarcube.BlockType.ARRAY,
+            text: "split [A] at every [B]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple,banana,pear"
+              },
+              B: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:","
+              },
+            },
+          },
+          "---",
+          {
+            opcode: "ifThen",
+            type: sugarcube.BlockType.REPORTER,
+            text: "if [A] then [B] else [C]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.BOOLEAN,
+              },
+              B: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              },
+              C: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"banana"
+              }
+            },
+          },
+          {
+            opcode: "convertToCase",
+            type: sugarcube.BlockType.REPORTER,
+            text: "make [A] [B]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              },
+              B: {
+                menu:"caseTypes"
+              },
+            },
+          },
+          {
+            opcode: "includes",
+            type: sugarcube.BlockType.BOOLEAN,
+            text: "[A] contains [B]",
+            arguments: {
+              A: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"apple"
+              },
+              B: {
+                type: sugarcube.ArgumentType.STRING,
+                defaultValue:"a"
               },
             },
           },
         ],
+        menus: {
+          caseTypes: {
+            items: [
+              "uppercase",
+              "lowercase",
+              "title-case"
+            ]
+          }
+        }
       };
     }
 
     length({A}) {
-      return 
+      return sugarcube.cast.toString(A).length;
     }
 
     join({A, B}) {
-      return 
+      return sugarcube.cast.toString(A) + sugarcube.cast.toString(B);
+    }
+
+    reverse({A}) {
+      return sugarcube.cast.toString(A).split("").reverse().join("");
+    }
+
+    repeat({A, B}) {
+      return sugarcube.cast.toString(A).repeat(sugarcube.cast.toNumber(B));
+    }
+
+    replace({A, B, C}) {
+      return sugarcube.cast.toString(B).replaceAll(sugarcube.cast.toString(A),sugarcube.cast.toString(C))
+    }
+
+    characterAt({A, B}) {
+      return sugarcube.cast.toString(B).charAt(sugarcube.cast.toNumber(A) - 1) || "";
+    }
+
+    charactersBetween({A, B, C}) {
+      return sugarcube.cast.toString(C).substring(sugarcube.cast.toNumber(A) - 1,sugarcube.cast.toNumber(B));
+    }
+
+    split({A, B}) {
+      return sugarcube.cast.toString(A).split(sugarcube.cast.toString(B));
+    }
+
+    ifThen({A, B, C}) {
+      return sugarcube.cast.toBoolean(A) ? sugarcube.cast.toString(B) : sugarcube.cast.toString(C);
+    }
+    
+    convertToCase({A, B}) {
+      A = sugarcube.cast.toString(A);
+
+      switch (B) {
+        case "uppercase":
+          return A.toUpperCase();
+
+        case "lowercase":
+          return A.toLowerCase();
+
+        case "title-case":
+          return A.replace(
+            /\w\S*/g,
+            function(txt) {
+              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            }
+          );
+      
+        default:
+          return A;
+      }
+    }
+
+    includes({A, B}) {
+      return sugarcube.cast.toString(A).includes(sugarcube.cast.toString(B));
     }
   }
 
