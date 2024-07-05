fe543bfc95b198b5e3e86410e6ec6563897ce6f4
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-05 15:33:30 -0400

    Ow! No offense.

--- src/engine/nodes/node.js
+++ src/engine/nodes/node.js
@@ -1,5 +1,25 @@
 (function() {
     coffeeEngine.classes.node = class {
+
+        #parent;
+
+        set parent(value) {
+            if (value.addChild) {
+                this.#parent.removeChild(this);
+
+                value.addChild(this);
+
+                this.#parent = value;
+            }
+            else {
+                console.error(`cannot set parent to ${String(value)}`)
+            }
+        }
+
+        get parent() {
+            return this.#parent;
+        }
+
         constructor() {
             this.children = [];
         }
