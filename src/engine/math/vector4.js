44b5054c4a3209012f4295ff1319ba2c7fb14bc6
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-05 00:59:57 -0400

    ve cah tah ford dee

--- src/engine/math/vector4.js
+++ src/engine/math/vector4.js
@@ -1,25 +1,26 @@
 (function() {
-    coffeeEngine.vector3 = class {
-        constructor(x,y,z) {
+    coffeeEngine.vector4 = class {
+        constructor(x,y,z,w) {
             this.x = x;
             this.y = y;
             this.z = z;
+            this.w = w;
         }
 
         add(b) {
-            return new coffeeEngine.vector3(this.x + b.x, this.y + b.y, this.z + b.z);
+            return new coffeeEngine.vector4(this.x + b.x, this.y + b.y, this.z + b.z, this.w + b.w);
         }
 
         sub(b) {
-            return new coffeeEngine.vector3(this.x - b.x, this.y - b.y, this.z - b.z);
+            return new coffeeEngine.vector4(this.x - b.x, this.y - b.y, this.z - b.z, this.w - b.w);
         }
 
         mul(b) {
-            return new coffeeEngine.vector3(this.x * b.x, this.y * b.y, this.z * b.z);
+            return new coffeeEngine.vector4(this.x * b.x, this.y * b.y, this.z * b.z, this.w * b.w);
         }
 
         div(b) {
-            return new coffeeEngine.vector3(this.x / b.x, this.y / b.y, this.z / b.z);
+            return new coffeeEngine.vector4(this.x / b.x, this.y / b.y, this.z / b.z, this.w / b.w);
         }
 
         length() {
@@ -27,44 +28,24 @@
         }
 
         lengthSquared() {
-            return Math.pow(this.x,2) + Math.pow(this.y,2) + Math.pow(this.z,2);
+            return Math.pow(this.x,2) + Math.pow(this.y,2) + Math.pow(this.z,2) + Math.pow(this.w,2);
         }
 
         normalize() {
             const length = this.length();
-            return this.div({x:length, y:length, z:length});
+            return this.div({x:length, y:length, z:length, w:length});
         }
 
         dot(b) {
             return this.mul(b).normalize();
         }
 
-        cross() {
-            return new coffeeEngine.vector3(
-                this.y*b.z - this.z*b.y,
-                this.z*b.x - this.x*b.z,
-                this.x*b.y - this.y*b.x
-            );
-        }
-
-        rotate(yaw,pitch,roll) {
-            const returned = new coffeeEngine.vector3(
-                this.z * Math.sin(yaw) + this.x * Math.cos(yaw),
-                this.y,
-                this.z * Math.cos(yaw) - this.x * Math.sin(yaw)
-            );
-            
-            returned.y = returned.z * Math.sin(pitch) + returned.y * Math.cos(pitch);
-            returned.z = returned.z * Math.cos(pitch) - returned.y * Math.sin(pitch);
-            
-            returned.x = returned.y * Math.sin(roll) + returned.x * Math.cos(roll);
-            returned.y = returned.y * Math.cos(roll) - returned.x * Math.sin(roll);
-
-            return returned;
+        rotate(matrix) {
+            return matrix.mulVector(this);
         }
 
         webGLValue() {
-            return [this.x,this.y,this.z];
+            return [this.x,this.y,this.z,this.w];
         }
     }
 })();
