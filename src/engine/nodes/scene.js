89b1f89a86d80da03ca6359b4bd81e9787bba031
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-05 16:10:01 -0400

    they were not like me.

--- src/engine/nodes/scene.js
+++ src/engine/nodes/scene.js
@@ -2,6 +2,43 @@
     coffeeEngine.sceneClass = class {
         constructor() {
             this.children = [];
+            this.events = {
+                update:[],
+                draw:[]
+            }
+        }
+
+        addEventListener(event,func) {
+            if (typeof this.events[event] != "object") return;
+
+            this.events[event].push(func);
+            return func;
+        }
+
+        hasEventListener(event,func) {
+            if (typeof this.events[event] != "object") return;
+
+            return this.events[event].includes(func);
+        }
+
+        removeEventListener(event,func) {
+            if (typeof this.events[event] != "object") return;
+
+            if (this.events[event].includes(func)) {
+                this.events[event].slice(this.events[event].indexOf(func));
+            }
+        }
+
+        update() {
+            this.events.update.forEach(event => {
+                event();
+            });
+        }
+
+        draw() {
+            this.events.draw.forEach(event => {
+                event();
+            });
         }
     }
 })();
