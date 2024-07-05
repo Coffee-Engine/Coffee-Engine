f1960890e241cbdaf44bb423d973ec4ffabaa473
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-05 01:32:57 -0400

    Gentlemen?

--- src/engine/math/matrix4.js
+++ src/engine/math/matrix4.js
@@ -53,12 +53,22 @@
             return this.multiply(rotator);
         }
 
-        multiplyVector(matrix,vector) {
+        multiply(matrix) {
+            //LETS GO WRITE OUT THE PROPER FORMULAS FOR MATRIX MULTIPLCATION FOR MAXIMUM SPEED POTENTIAL! YEAH!!!!!!!!!
+            new coffeeEngine.matrix4([
+                1,0,0,x,
+                0,1,0,y,
+                0,0,1,z,
+                0,0,0,1
+            ])
+        }
+
+        multiplyVector(vector) {
             const returned = new coffeeEngine.vector4(0,0,0,0);
-            returned.x = vector.x * matrix.contents[0][0] + vector.y * matrix.contents[1][0] + vector.z * matrix.contents[2][0] + vector.w * matrix.contents[3][0];
-            returned.y = vector.x * matrix.contents[0][1] + vector.y * matrix.contents[1][1] + vector.z * matrix.contents[2][1] + vector.w * matrix.contents[3][1];
-            returned.z = vector.x * matrix.contents[0][2] + vector.y * matrix.contents[1][2] + vector.z * matrix.contents[2][2] + vector.w * matrix.contents[3][2];
-            returned.w = vector.x * matrix.contents[0][3] + vector.y * matrix.contents[1][3] + vector.z * matrix.contents[2][3] + vector.w * matrix.contents[3][3];
+            returned.x = vector.x * this.contents[0][0] + vector.y * this.contents[1][0] + vector.z * this.contents[2][0] + vector.w * this.contents[3][0];
+            returned.y = vector.x * this.contents[0][1] + vector.y * this.contents[1][1] + vector.z * this.contents[2][1] + vector.w * this.contents[3][1];
+            returned.z = vector.x * this.contents[0][2] + vector.y * this.contents[1][2] + vector.z * this.contents[2][2] + vector.w * this.contents[3][2];
+            returned.w = vector.x * this.contents[0][3] + vector.y * this.contents[1][3] + vector.z * this.contents[2][3] + vector.w * this.contents[3][3];
             return returned;
         }
     }
