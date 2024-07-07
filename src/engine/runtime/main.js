35e80ef25b5a33bbef39c61358c6b4d76d08ca14
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-07 02:27:06 -0400

    Silly :P

--- src/engine/runtime/main.js
+++ src/engine/runtime/main.js
@@ -1,6 +1,10 @@
 (function() {
     coffeeEngine.runtime.currentScene = new coffeeEngine.sceneClass();
 
+    coffeeEngine.runtime.paused = false;
+    coffeeEngine.runtime.lastTick = Date.now();
+    coffeeEngine.runtime.deltaTime = 0.016;
+
     coffeeEngine.runtime.update = (deltaTime) => {
         if (coffeeEngine.runtime.currentScene) {
             coffeeEngine.runtime.currentScene.update(deltaTime)
@@ -11,5 +15,10 @@
         if (coffeeEngine.runtime.currentScene) {
             coffeeEngine.runtime.currentScene.draw();
         }
+    }
+
+    coffeeEngine.runtime.updateDelta = () => {
+        coffeeEngine.runtime.deltaTime = (Date.now() - coffeeEngine.runtime.lastTick) / 1000;
+        coffeeEngine.runtime.lastTick = Date.now();
     }
 })();
