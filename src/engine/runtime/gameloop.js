d4416e5818b112f7a9ac01572442c42617a3d8bb
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-07 15:05:26 -0400

    Fradamama loopa doopa

---
+++ src/engine/runtime/gameloop.js
@@ -0,0 +1,17 @@
+(function() {
+    coffeeEngine.runtime.frameloopFunction = () => {
+        coffeeEngine.runtime.updateDelta();
+        if (!coffeeEngine.runtime.paused) {
+            coffeeEngine.runtime.update(coffeeEngine.runtime.deltaTime);
+            coffeeEngine.runtime.draw();
+        }
+    };
+
+    coffeeEngine.runtime.startFrameLoop = (framerate) => {
+        if (coffeeEngine.runtime.frameloop) {
+            clearInterval(coffeeEngine.runtime.frameloop);
+        }
+
+        coffeeEngine.runtime.frameloop = setInterval(coffeeEngine.runtime.frameloopFunction, (1/framerate) * 1000);
+    }
+})();
