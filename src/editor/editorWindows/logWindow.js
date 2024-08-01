f43b8fcf7deeb8efc889a6839f63c7b85fc9bf70
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-31 23:38:33 -0400

    He started threatening people with a boiling pot of soup

--- src/editor/editorWindows/logWindow.js
+++ src/editor/editorWindows/logWindow.js
@@ -14,6 +14,8 @@
                 displayEl.style.width = "100%";
 
                 container.appendChild(displayEl);
+
+                this.oldLog(...stuff);
             }
 
             window.warn = (...stuff) => {
@@ -22,8 +24,11 @@
 
                 displayEl.style.width = "100%";
                 displayEl.style.backgroundColor = "var(--warn)";
+                displayEl.style.color = "var(--warn-text)";
 
                 container.appendChild(displayEl);
+
+                this.oldWarn(...stuff);
             }
 
             console.error = (...stuff) => {
@@ -32,17 +37,20 @@
 
                 displayEl.style.width = "100%";
                 displayEl.style.backgroundColor = "var(--error)";
+                displayEl.style.color = "var(--error-text)";
 
                 container.appendChild(displayEl);
+
+                this.oldError(...stuff);
             }
 
             window.onerror = (event, source, lineno, colno, error) => {
-                console.log(arguments)
                 const displayEl = document.createElement("div");
                 displayEl.innerText = error;
 
                 displayEl.style.width = "100%";
                 displayEl.style.backgroundColor = "var(--error)";
+                displayEl.style.color = "var(--error-text)";
 
                 container.appendChild(displayEl);
             }
@@ -52,6 +60,11 @@
         }
 
         dispose() {
+            console.log = this.oldLog;
+            console.warn = this.oldWarn;
+            console.error = this.oldError;
+
+            window.onerror = () => {};
         }
     };
 })();
