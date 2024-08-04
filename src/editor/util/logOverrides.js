9dfd5a6143eb47d5a3ae43628ee6cd1e9dd889bb
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-08-03 23:05:22 -0400

    schududous.

--- src/editor/util/logOverrides.js
+++ src/editor/util/logOverrides.js
@@ -2,4 +2,41 @@
     const oCLOG = console.log;
     const oCWARN = console.warn;
     const oCERROR = console.error;
+    const oCINFO = console.info;
+
+    console.log = (...stuff) => {
+        coffeeEngine.sendEvent("consoleUpdate",{
+            type:"log",
+            info:stuff
+        });
+
+        oCLOG(...stuff);
+    }
+
+    console.warn = (...stuff) => {
+        coffeeEngine.sendEvent("consoleUpdate",{
+            type:"warn",
+            info:stuff
+        });
+
+        oCWARN(...stuff);
+    }
+
+    console.error = (...stuff) => {
+        coffeeEngine.sendEvent("consoleUpdate",{
+            type:"error",
+            info:stuff
+        });
+
+        oCERROR(...stuff);
+    }
+
+    console.info = (...stuff) => {
+        coffeeEngine.sendEvent("consoleUpdate",{
+            type:"info",
+            info:stuff
+        });
+
+        oCINFO(...stuff);
+    }
 }
--- src/engine/engineInit.js
+++ src/engine/engineInit.js
@@ -1,6 +1,37 @@
 const coffeeEngine = {
+    events: {
+        consoleUpdate:[]
+    },
     runtime: {},
     classes: {},
     resources: {},
     renderer: {},
+    addEventListener:(event, func) => {
+        if (typeof coffeeEngine.events[event] != "object") return;
+
+        coffeeEngine.events[event].push(func);
+        return func;
+    },
+
+    hasEventListener:(event, func) => {
+        if (typeof coffeeEngine.events[event] != "object") return;
+
+        return coffeeEngine.events[event].includes(func);
+    },
+
+    removeEventListener:(event, func) => {
+        if (typeof coffeeEngine.events[event] != "object") return;
+
+        if (coffeeEngine.events[event].includes(func)) {
+            coffeeEngine.events[event].slice(coffeeEngine.events[event].indexOf(func));
+        }
+    },
+
+    sendEvent:(event,data) => {
+        if (typeof coffeeEngine.events[event] != "object") return;
+
+        coffeeEngine.events[event].forEach(event => {
+            event(data);
+        });
+    }
 };
