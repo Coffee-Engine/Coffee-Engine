9f3c5d12eb2cfb8ee5a36aed6087fd4c945dc43a
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-08-05 22:17:48 -0400

    testcat

--- src/editor/codeEditors/sugarcube/defaultBlocks/testCat.js
+++ src/editor/codeEditors/sugarcube/defaultBlocks/testCat.js
@@ -247,6 +247,12 @@
                         items: "dynamicMenuFunc",
                     },
                 },
+                mutators: {
+
+                },
+                fields: {
+
+                }
             };
         }
 
