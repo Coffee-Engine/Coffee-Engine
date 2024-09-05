150f8b7a811531603bf89ee6a703af030a02d13e
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-09-05 18:13:22 -0400

    I'm useless but not for long

--- src/editor/codeEditors/sugarcube/defaultBlocks/variables.js
+++ src/editor/codeEditors/sugarcube/defaultBlocks/variables.js
@@ -29,6 +29,8 @@
         openVariableMenu() {
             const createdWindow = new editor.windows.variable(400,300);
             createdWindow.__moveToTop();
+            createdWindow.x = window.innerWidth / 2 - 200;
+            createdWindow.y = window.innerHeight / 2 - 150;
             createdWindow.variableType = "variable"
         }
 
