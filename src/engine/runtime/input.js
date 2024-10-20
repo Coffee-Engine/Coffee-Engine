fff5011cdcfd3f3fc624a415fdbe7fbb23bb580f
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-10-20 09:25:19 -0400

    I'm going rabid

--- src/engine/runtime/input.js
+++ src/engine/runtime/input.js
@@ -1,6 +1,14 @@
 (function () {
     coffeeEngine.inputs = {
         keys: {},
+        mouse: {},
+        //gamepad objects
+        gamepads: {
+            0:{},
+            1:{},
+            2:{},
+            3:{},
+        },
     };
 
     window.addEventListener("keydown", (event) => {
@@ -9,5 +17,36 @@
 
     window.addEventListener("keyup", (event) => {
         coffeeEngine.inputs.keys[event.key] = false;
+    });
+
+    window.addEventListener("mousedown", (event) => {
+        coffeeEngine.inputs.mouse[event.button] = true;
+    });
+
+    window.addEventListener("mouseup", (event) => {
+        coffeeEngine.inputs.mouse[event.button] = false;
+    });
+
+    //just a helper function
+    coffeeEngine.inputs.initilizeGamepad = (gamepad) => {
+        if (!gamepad) return;
+        
+        coffeeEngine.inputs.gamepads[gamepad.index] = {
+            object:gamepad,
+            id:gamepad.id,
+            mapping:gamepad.mapping,
+            buttons:gamepad.buttons,
+            axes:gamepad.axes,
+        }
+    }
+
+    //Gamepad initilization
+    navigator.getGamepads = navigator.getGamepads || navigator.webkitGetGamepads;
+    navigator.getGamepads().forEach(gamepad => {
+        coffeeEngine.inputs.initilizeGamepad(gamepad);
+    });
+
+    window.addEventListener("gamepadconnected", (e) => {
+        coffeeEngine.inputs.initilizeGamepad(e.gamepad);
     });
 })();
