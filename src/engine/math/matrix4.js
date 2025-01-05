bf8eeb311a880c2ab55bf1437987639714f7b94f
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2025-01-04 21:44:13 -0500

    Getting closer

--- src/engine/math/matrix4.js
+++ src/engine/math/matrix4.js
@@ -33,7 +33,38 @@
                 [-Math.sin(rad),Math.cos(rad),0,0],
                 [0,0,1,0],
                 [0,0,0,1]
-            ])
+            ]);
+            return this.multiply(rotator);
+        }
+        /*
+
+1.0 - 2.0*y*y - 2.0*z*z, 2.0*x*y - 2.0*z*w, 2.0*x*z + 2.0*y*w, 0.0,
+2.0*x*y + 2.0*z*w, 1.0 - 2.0*x*x - 2.0*z*z, 2.0*y*z - 2.0*x*w, 0.0,
+2.0*x*z - 2.0*y*w, 2.0*y*z + 2.0*x*w, 1.0 - 2.0*x*x - 2.0*y*y, 0.0,
+0.0, 0.0, 0.0, 1.0
+        */
+
+        rotateQuaternion(qx,qy,qz,qw) {
+            //const rotator = new coffeeEngine.matrix4([
+            //    [1.0 - 2.0*y*y - 2.0*z*z, 2.0*x*y - 2.0*z*w, 2.0*x*z + 2.0*y*w, 0.0,],
+            //    [2.0*x*y + 2.0*z*w, 1.0 - 2.0*x*x - 2.0*z*z, 2.0*y*z - 2.0*x*w, 0.0,],
+            //    [2.0*x*z - 2.0*y*w, 2.0*y*z + 2.0*x*w, 1.0 - 2.0*x*x - 2.0*y*y, 0.0,],
+            //    [0.0, 0.0, 0.0, 1.0]
+            //]);
+            var te = this.elements;
+
+            var x = qx, y = qy, z = qz, w = qw;
+            var x2 = x + x, y2 = y + y, z2 = z + z;
+            var xx = x * x2, xy = x * y2, xz = x * z2;
+            var yy = y * y2, yz = y * z2, zz = z * z2;
+            var wx = w * x2, wy = w * y2, wz = w * z2;
+            
+            const rotator = new coffeeEngine.matrix4([
+                [1 - ( yy + zz ),xy + wz,xz - wy,0],
+                [xy - wz,1 - ( xx + zz ),yz + wx,0],
+                [xz + wy,yz - wx,1 - ( xx + yy ),0],
+                [0,0,0,1]
+            ]);
             return this.multiply(rotator);
         }
 
--- src/engine/resources/gltf.js
+++ src/engine/resources/gltf.js
@@ -21,15 +21,22 @@
             //Loop through the bytes of the positions
             for (let byteID = bufferView.byteOffset; byteID < (bufferView.byteOffset + bufferView.byteLength); byteID+=stepSize) {
                 //Coordinites
-                for (let x=0; x < defaultSize; x++) {
-                    returner.push(type[0](BINPartition,byteID + (x*type[1]),false));
+                if (defaultSize > 1) {
+                    const conjoined = []
+                    for (let x=0; x < defaultSize; x++) {
+                        conjoined.push(type[0](BINPartition,byteID + (x*type[1]),false));
+                    }
+                    returner.push(conjoined);
+                }
+                else {
+                    returner.push(type[0](BINPartition,byteID,false));
                 }
             }
         }
-        else returner = (new Array(standard.count*defaultSize)).fill(1);
-
-        console.log(name);
-        console.log(returner);
+        else {
+            if (defaultSize > 1) returner = (new Array(standard.count)).fill(new Array(defaultSize).fill(1));
+            else returner = (new Array(standard.count*defaultSize)).fill(1);
+        }
 
         return returner;
     }
@@ -62,8 +69,7 @@
         const bufferViews = JSONPartition.bufferViews;
 
         //Mesh Reader
-        const readMesh = (mesh) => {
-            console.log(mesh);
+        const readMesh = (transform, mesh) => {
             mesh.primitives.forEach(primitive => {
                 const primitiveGeo = {}
                 const attributes = primitive.attributes;
@@ -74,26 +80,72 @@
                 primitiveGeo.a_texCoord = getAttributeData("TEXCOORD_0",attributes,accessors,bufferViews,standard,BINPartition,2);
                 primitiveGeo.a_normal = getAttributeData("NORMAL",attributes,accessors,bufferViews,standard,BINPartition,3);
                 primitiveGeo.a_color = getAttributeData("COLOR_n",attributes,accessors,bufferViews,standard,BINPartition,4);
+
+                for (let id = 0; id < primitiveGeo.a_position.length; id++) {
+                    const transformed = transform.multiplyVector({
+                        x:primitiveGeo.a_position[id][0],
+                        y:primitiveGeo.a_position[id][1],
+                        z:primitiveGeo.a_position[id][2],
+                        w:1
+                    });
+
+                    primitiveGeo.a_position[id][0] = transformed.x;
+                    primitiveGeo.a_position[id][1] = transformed.y;
+                    primitiveGeo.a_position[id][2] = transformed.z;
+                }
                 
                 const indicies = getAttributeData("indices",primitive,accessors,bufferViews,standard,BINPartition,1);
-                console.log(indicies);
-            })
+                
+                const constructedSubmesh = {
+                    a_position:[],
+                    a_texCoord:[],
+                    a_normal:[],
+                    a_color:[],
+                };
+
+                indicies.forEach(indice => {
+                    constructedSubmesh.a_position.push(primitiveGeo.a_position[indice],1);
+                    constructedSubmesh.a_texCoord.push(primitiveGeo.a_texCoord[indice]);
+                    constructedSubmesh.a_normal.push(primitiveGeo.a_normal[indice]);
+                    constructedSubmesh.a_color.push(primitiveGeo.a_color[indice]);
+                });
+
+                data.push(constructedSubmesh);
+                pointCount.push(indicies.length);
+                
+                const dataLength = data.length - 1;
+                data[dataLength].a_position = new Float32Array(data[dataLength].a_position.flat(4));
+                data[dataLength].a_texCoord = new Float32Array(data[dataLength].a_texCoord.flat(4));
+                data[dataLength].a_normal = new Float32Array(data[dataLength].a_normal.flat(4));
+                data[dataLength].a_color = new Float32Array(data[dataLength].a_color.flat(4));
+            });
         }
 
         //Find our mesh
-        currentScene.nodes.forEach(nodeID => {
-            const node = JSONPartition.nodes[nodeID];
-            if (node.mesh !== undefined) {
-                //If we are a mesh parse our primitives
-                readMesh(JSONPartition.meshes[node.mesh]);
-            }
-        });
+        console.log(JSONPartition)
+        const readNodes = (matrix,nodeArray) => {
+            nodeArray.forEach(nodeID => {
+                const node = JSONPartition.nodes[nodeID];
 
-        //Flatten em
-        //const dataLength = data.length - 1;
-        //data[dataLength].a_position = new Float32Array(data[dataLength].a_position.flat(4));
-        //data[dataLength].a_texCoord = new Float32Array(data[dataLength].a_texCoord.flat(4));
-        //data[dataLength].a_normal = new Float32Array(data[dataLength].a_normal.flat(4));
-        //data[dataLength].a_color = new Float32Array(data[dataLength].a_color.flat(4));
+                let localMatrix = coffeeEngine.matrix4.identity();
+                console.log(node);
+                if (node.scale) localMatrix = localMatrix.scale(node.scale[0],node.scale[1],node.scale[2]);
+                if (node.rotation) localMatrix = localMatrix.rotateQuaternion(node.rotation[0],node.rotation[1],node.rotation[2],node.rotation[3]);
+                if (node.translation) localMatrix = localMatrix.translate(node.translation[0],node.translation[1],node.translation[2]);
+
+                const multipliedMatrix = matrix.multiply(localMatrix);
+
+                if (node.mesh !== undefined) {
+                    //If we are a mesh parse our primitives
+                    readMesh(multipliedMatrix,JSONPartition.meshes[node.mesh]);
+                }
+
+                if (node.children) {
+                    readNodes(multipliedMatrix,node.children);
+                }
+            });
+        }
+
+        readNodes(coffeeEngine.matrix4.identity(),currentScene.nodes);
     };
 })();
