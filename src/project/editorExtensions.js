708a2c2751a77213bd3f6abea779354cbd841dfc
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-01-17 07:40:14 -0500

    Make editorScripts tag in extension.json

--- src/project/editorExtensions.js
+++ src/project/editorExtensions.js
@@ -44,13 +44,20 @@
             //Read our extension.json
             this.fileReader.onload = () => {
                 project.extensions.storage[extensionID] = JSON.parse(this.fileReader.result);
-                this.loadScripts(`extensions/${extensionID}/`, project.extensions.storage[extensionID].scripts);
+
+                //Go through needed script directories
+                const myStorage = project.extensions.storage[extensionID];
+                this.loadScripts(`extensions/${extensionID}/`, myStorage.scripts);
+                if (coffeeEngine.isEditor) this.loadScripts(`extensions/${extensionID}/`, myStorage.editorScripts);
             };
 
             this.fileReader.readAsText(file);
         }
 
         async loadScripts(path, scriptArray) {
+            //Just make sure its an array!
+            scriptArray = scriptArray || [];
+
             for (let index = 0; index < scriptArray.length; index++) {
                 await new Promise((resolve, reject) => {
                     //Load our script
@@ -66,6 +73,10 @@
 
                             resolve();
                         };
+
+                        this.fileReader.onerror = () => {
+                            reject(`Something happened with file "${path}${scriptArray[index]}"`);
+                        }
 
                         this.fileReader.readAsText(fileData);
                     });
