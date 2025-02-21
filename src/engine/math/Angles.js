19fb9223031531d05a5be14e971482f0772bf0e5
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-02-21 07:48:10 -0500

    Fix magic function for magic numbers

--- src/engine/math/Angles.js
+++ src/engine/math/Angles.js
@@ -2,6 +2,6 @@
     //? Precalc division is slow
     const radianMagicNumber = (Math.PI/180);
     const degreeMagicNumber = (180/Math.PI);
-    coffeeEngine.deg2Rad = () => rad => rad * radianMagicNumber;
+    coffeeEngine.deg2Rad = deg => deg * radianMagicNumber;
     coffeeEngine.rad2Deg = rad => rad * degreeMagicNumber;
 })();
