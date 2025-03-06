64c28414709103aecfcd0b5c25cba2c2872f9446
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-03-06 08:41:17 -0500

    project settings window inner stuff.

--- src/editor/editorWindows/projectManager.js
+++ src/editor/editorWindows/projectManager.js
@@ -1,7 +1,30 @@
 (function() {
     editor.windows.projectManager = class extends editor.windows.base {
         init (container) {
+            //Make title bar
             this.title = editor.language["editor.window.projectManager"];
+
+            this.sideBar = document.createElement("div");
+            this.configurationArea = document.createElement("div");
+            
+            //Style our container and add our areas
+            container.style.display = "grid";
+            container.style.gridTemplateColumns = "10% auto";
+
+            container.appendChild(this.sideBar);
+            container.appendChild(this.configurationArea);
+
+            //style our sidebar
+            this.sideBar.style.background = "var(--background-2)";
+            this.sideBar.style.borderRight = "4px solid var(--background-4)";
+            this.configurationArea.style.borderLeft = "4px solid var(--background-4)";
+
+            Object.keys(project.settingConfigs).forEach(key => {
+                const button = document.createElement("button");
+                button.innerText = key;
+                button.style.width = "100%";
+                this.sideBar.appendChild(button);
+            })
         }
     }
 
---
+++ src/project/projectSettings.js
@@ -0,0 +1,14 @@
+project.settingConfigs = {
+    scene: {
+        type: "list",
+        contents: [
+            {
+                type:"label",
+                text:"Hi"
+            }
+        ]
+    },
+    collision: {
+        type: "collisionArray"
+    }
+}
