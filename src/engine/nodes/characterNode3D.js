3f10dc66befee83020f3d76a9c8dd5a1c6784a94
Dr. Cleve <alexclevenger2007@gmail.com>
2025-03-10 17:47:48 -0400

    start work on character node

---
+++ src/engine/nodes/characterNode3D.js
@@ -0,0 +1,23 @@
+(function () {
+    class node extends coffeeEngine.getNode("Node3D") {
+        collisionGroup = "default";
+
+        update(deltaTime) {
+            super.update(deltaTime);
+            //get my collisions
+            const output = this.detectCollisions();
+
+            if (output.length > 0) {
+                //Find outputs and push object out of trouble
+                for (let outputID in output) {
+                    const outputObject = output[outputID];
+                    this.position.x -= outputObject.pushVector.x * outputObject.pushLength;
+                    this.position.y -= outputObject.pushVector.y * outputObject.pushLength;
+                    this.position.z -= outputObject.pushVector.z * outputObject.pushLength;
+                }
+            }
+        }
+    }
+
+    coffeeEngine.registerNode(node, "CharacterNode3D", "Node3D");
+})();
