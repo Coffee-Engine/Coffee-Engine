1c7bef7aeec873a8e997eabd9818482f2f073ee5
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-04-04 10:07:05 -0400

    Get properties via new @editor ["property name"] format

--- src/engine/resources/behaviorManager.js
+++ src/engine/resources/behaviorManager.js
@@ -3,6 +3,10 @@
         storage: {},
         loadedFiles: {},
         lastLoaded: {},
+
+        CUGIPropertyRegex: /@editor\s*\(\w*\)\s*\w*[\s=;]*.*;/g,
+        CUGITypeRegex: /@editor\s*\(\w*(?=\))/g,
+        
         register: (name, classObj) => {
             coffeeEngine.behaviorManager.storage[name] = classObj;
             coffeeEngine.behaviorManager.lastLoaded = classObj;
@@ -27,12 +31,15 @@
                             //Make the script and check for it being loaded
                             const script = document.createElement("script");
 
+                            //get CUGI properties
+                            const properties = coffeeEngine.behaviorManager.parseProperties(fileReader.result.match(coffeeEngine.behaviorManager.CUGIPropertyRegex) || []);
+
                             //Set the inner text and add the element.
                             script.innerHTML = `(function() {\n${fileReader.result}\n})();`;
                             document.body.appendChild(script);
 
                             //Save it
-                            coffeeEngine.behaviorManager.loadedFiles[filePath] = coffeeEngine.behaviorManager.lastLoaded;
+                            coffeeEngine.behaviorManager.loadedFiles[filePath] = { behavior: coffeeEngine.behaviorManager.lastLoaded, properties: properties};
                             resolve(coffeeEngine.behaviorManager.loadedFiles[filePath]);
                         };
 
@@ -43,5 +50,57 @@
                     });
             });
         },
+
+        //The not as impressive younger cousin of behavior from file.
+        behaviorPropertiesFromFile: (filePath) => {
+            //We might as well return the actual behavior if we do have it
+            if (!coffeeEngine.isEditor) return coffeeEngine.behaviorManager.behaviorFromFile();
+
+            return new Promise((resolve, reject) => {
+                //check if its loaded
+                if (coffeeEngine.behaviorManager.loadedFiles[filePath]) resolve(coffeeEngine.behaviorManager.loadedFiles[filePath]);
+
+                //if not load it
+                project
+                    .getFile(filePath)
+                    .then((file) => {
+                        const fileReader = new FileReader();
+
+                        //when the file is parsed turn it into a script
+                        fileReader.onload = () => {
+                            //get CUGI properties
+                            const properties = coffeeEngine.behaviorManager.parseProperties(fileReader.result.match(coffeeEngine.behaviorManager.CUGIPropertyRegex) || []);
+
+                            //Save it
+                            coffeeEngine.behaviorManager.loadedFiles[filePath] = { path: filePath, properties: properties};
+                            resolve(coffeeEngine.behaviorManager.loadedFiles[filePath]);
+                        };
+
+                        fileReader.readAsText(file);
+                    })
+                    .catch((error) => {
+                        reject(error);
+                    });
+            })
+        },
+
+        parseProperties: (propertyArray) => {
+            for (let propertyID in propertyArray) {
+                const propertyString = propertyArray[propertyID];
+
+                //Get the property type
+                let propertyType = propertyString.match(coffeeEngine.behaviorManager.CUGITypeRegex)[0];
+                propertyType = propertyType.split(/@editor\s*\(/g)[1].replace(/\)\s*\w*[\s=;]*.*;/g,"").trim();
+                
+                //now lets isolate the property name;
+                let propertyName = propertyString.match(/".*(?=")/g)[0].replace('"',"").replaceAll(/\\(?=.)/g, "");
+
+                //Then throw it in the array again
+                propertyArray[propertyID] = { name: propertyName, type: propertyType.toLowerCase() };
+            }
+
+            //Churn out our new array
+            return propertyArray;
+        }
     };
 })();
