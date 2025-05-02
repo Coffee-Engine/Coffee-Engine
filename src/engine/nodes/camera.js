e159e1319feb9fb501f4d43c6981cf72ba6aed9d
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-05-02 08:02:32 -0400

    Start work on deferring the camera to the scene.

--- src/engine/nodes/camera.js
+++ src/engine/nodes/camera.js
@@ -1,5 +1,4 @@
 (function () {
-
     class cameraNode extends coffeeEngine.getNode("Node3D") {
         activeCamera = false;
         fov = 90;
@@ -42,38 +41,45 @@
                     const audioListener = coffeeEngine.audio.context.listener;
 
                     //Update our camera data
-                    const cameraRender = this.matrix.multiply(this.parent.mixedMatrix.inverse());
-                    cameraData.transform = cameraRender.webGLValue();
-                    cameraData.unflattenedTransform = this.mixedMatrix;
-                    cameraData.projection = coffeeEngine.matrix4.projection(this.fov, 1, 0.01, 1000).webGLValue();
-                    cameraData.wFactor = [(this.orthographic) ? 0 : 1, this.zoom, this.nearPlane];
-                    cameraData.aspectRatio = canvas.width / canvas.height;
+                    //const cameraRender = this.matrix.multiply(this.parent.mixedMatrix.inverse());
+                    //cameraData.transform = cameraRender.webGLValue();
+                    //cameraData.unflattenedTransform = this.mixedMatrix;
+                    //cameraData.projection = coffeeEngine.matrix4.projection(this.fov, 1, 0.01, 1000).webGLValue();
+                    //cameraData.wFactor = [(this.orthographic) ? 0 : 1, this.zoom, this.nearPlane];
+                    //cameraData.aspectRatio = canvas.width / canvas.height;
 
-                    const translatedRender = this.mixedMatrix.getTranslation();
-                    cameraData.position.x = translatedRender.x;
-                    cameraData.position.y = translatedRender.y;
-                    cameraData.position.z = translatedRender.z;
+                    //const translatedRender = this.mixedMatrix.getTranslation();
+                    //cameraData.position.x = translatedRender.x;
+                    //cameraData.position.y = translatedRender.y;
+                    //cameraData.position.z = translatedRender.z;
 
-                    coffeeEngine.renderer.cameraData.cameraRotationEul.x = -this.rotation.y;
-                    coffeeEngine.renderer.cameraData.cameraRotationEul.y = -this.rotation.x;
-                    coffeeEngine.renderer.cameraData.cameraRotationEul.z = -this.rotation.z;
+                    //coffeeEngine.renderer.cameraData.cameraRotationEul.x = -this.rotation.y;
+                    //coffeeEngine.renderer.cameraData.cameraRotationEul.y = -this.rotation.x;
+                    //coffeeEngine.renderer.cameraData.cameraRotationEul.z = -this.rotation.z;
 
-                    //Set audio data
-                    if (audioListener.positionX) audioListener.positionX.value = -translatedWorld.x;
-                    if (audioListener.positionY) audioListener.positionY.value = -translatedWorld.y;
-                    if (audioListener.positionZ) audioListener.positionZ.value = (this.orthographic) ? 0 : -translatedWorld.z;
+                    ////Set audio data
+                    //if (audioListener.positionX) audioListener.positionX.value = -translatedWorld.x;
+                    //if (audioListener.positionY) audioListener.positionY.value = -translatedWorld.y;
+                    //if (audioListener.positionZ) audioListener.positionZ.value = (this.orthographic) ? 0 : -translatedWorld.z;
 
-                    const rotationData = this.mixedMatrix.getRotation();
+                    //const rotationData = this.mixedMatrix.getRotation();
 
-                    //Now our rotations
-                    if (audioListener.forwardX) audioListener.forwardX.value = rotationData.contents[2][0];
-                    if (audioListener.forwardY) audioListener.forwardY.value = rotationData.contents[2][1];
-                    if (audioListener.forwardZ) audioListener.forwardZ.value = rotationData.contents[2][2];
+                    ////Now our rotations
+                    //if (audioListener.forwardX) audioListener.forwardX.value = rotationData.contents[2][0];
+                    //if (audioListener.forwardY) audioListener.forwardY.value = rotationData.contents[2][1];
+                    //if (audioListener.forwardZ) audioListener.forwardZ.value = rotationData.contents[2][2];
 
-                    //Now our rotations
-                    if (audioListener.upX) audioListener.upX.value = rotationData.contents[1][0];
-                    if (audioListener.upY) audioListener.upY.value = rotationData.contents[1][1];
-                    if (audioListener.upZ) audioListener.upZ.value = rotationData.contents[1][2];
+                    ////Now our rotations
+                    //if (audioListener.upX) audioListener.upX.value = rotationData.contents[1][0];
+                    //if (audioListener.upY) audioListener.upY.value = rotationData.contents[1][1];
+                    //if (audioListener.upZ) audioListener.upZ.value = rotationData.contents[1][2];
+
+                    return {
+                        type: "CAMERA_DAT",
+                        render: cameraRender,
+                        position: translatedRender,
+                        rotationEuler: { x: -this.rotation.y, y: -this.rotation.x, z: -this.rotation.z }
+                    }
                 }
             }
         }
