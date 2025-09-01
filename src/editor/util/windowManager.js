646e61da0e8a35a0a33e75df278ad7ce98f669d4
Dr. Cleve <76855369+David-Orangemoon@users.noreply.github.com>
2025-09-01 14:14:48 -0400

    Fix styling of window manager

--- src/editor/util/windowManager.js
+++ src/editor/util/windowManager.js
@@ -1,55 +1,57 @@
-//This will contain the editor's windows
-editor.windowLayer = 0;
-editor.windows = {};
+(function() {
+    //This will contain the editor's windows
+    editor.windowLayer = 0;
+    editor.windows = {};
 
-editor.windows.existing = {};
+    editor.windows.existing = {};
 
-//this is the base window class
-editor.windows.__Serialization = {
-    register: (classOBJ, id, jsonData) => {
-        //Extra json can be used to add special data to windows like only allowing 1 to be open at a time.
-        editor.windows.__Serialization.all[id] = classOBJ; //{object: classOBJ, extraJson:jsonData};
-        editor.windows.__Serialization.data[id] = jsonData || {};
-    },
+    //this is the base window class
+    editor.windows.__Serialization = {
+        register: (classOBJ, id, jsonData) => {
+            //Extra json can be used to add special data to windows like only allowing 1 to be open at a time.
+            editor.windows.__Serialization.all[id] = classOBJ; //{object: classOBJ, extraJson:jsonData};
+            editor.windows.__Serialization.data[id] = jsonData || {};
+        },
 
-    deregister: (id) => {
-        if (!editor.windows.__Serialization[id]) return;
+        deregister: (id) => {
+            if (!editor.windows.__Serialization[id]) return;
 
-        if (editor.windows.existing[id]) {
-            editor.windows.existing[id].forEach(window => {
-                window._dispose();
-            })
-        }
-        delete editor.windows.__Serialization.all[id];
-        delete editor.windows.__Serialization.data[id];
-    },
+            if (editor.windows.existing[id]) {
+                editor.windows.existing[id].forEach(window => {
+                    window._dispose();
+                })
+            }
+            delete editor.windows.__Serialization.all[id];
+            delete editor.windows.__Serialization.data[id];
+        },
 
-    find: (windowOBJ) => {
-        //Check to make sure the window has tabs. if it does serialize those too.
-        if (windowOBJ.tabs && windowOBJ.tabs.length > 1) {
-            const serializedArray = [];
-            for (windowTabIndex in windowOBJ.tabs) {
-                const windowTab = windowOBJ.tabs[windowTabIndex].owner;
+        find: (windowOBJ) => {
+            //Check to make sure the window has tabs. if it does serialize those too.
+            if (windowOBJ.tabs && windowOBJ.tabs.length > 1) {
+                const serializedArray = [];
+                for (windowTabIndex in windowOBJ.tabs) {
+                    const windowTab = windowOBJ.tabs[windowTabIndex].owner;
 
-                //Serialize the tab
-                for (const key in editor.windows.__Serialization.all) {
-                    const constructor = editor.windows.__Serialization.all[key];
-                    if (constructor == windowTab.constructor) {
-                        serializedArray.push(key);
-                        break;
+                    //Serialize the tab
+                    for (const key in editor.windows.__Serialization.all) {
+                        const constructor = editor.windows.__Serialization.all[key];
+                        if (constructor == windowTab.constructor) {
+                            serializedArray.push(key);
+                            break;
+                        }
                     }
                 }
-            }
 
-            return serializedArray;
-        } else {
-            for (const key in editor.windows.__Serialization.all) {
-                const constructor = editor.windows.__Serialization.all[key];
-                if (constructor == windowOBJ.constructor) return key;
+                return serializedArray;
+            } else {
+                for (const key in editor.windows.__Serialization.all) {
+                    const constructor = editor.windows.__Serialization.all[key];
+                    if (constructor == windowOBJ.constructor) return key;
+                }
             }
-        }
-    },
+        },
 
-    all: {},
-    data: {},
-};
+        all: {},
+        data: {},
+    };
+})();
