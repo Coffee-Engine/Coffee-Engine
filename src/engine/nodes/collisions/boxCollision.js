907bdf6ba0552404498d95fff91c190e4ebe7ad4
Tiramisu <71141838+AlexSchoolOH@users.noreply.github.com>
2025-11-06 13:52:50 -0500

    Get boxCollision 3d rendering in new renderer

--- src/engine/nodes/collisions/boxCollision.js
+++ src/engine/nodes/collisions/boxCollision.js
@@ -2,27 +2,16 @@
     class node extends coffeeEngine.getNode("Collision3D") {
         collision = new coffeeEngine.SAT.OBB();
 
-        draw(drawID) {
-            super.draw();
-            //Editor display
-            if (coffeeEngine.isEditor) {
-                if (editor.lastSelectedNode != this) return;
-                //Get our shader
-                const shader = coffeeEngine.renderer.mainShaders.editorShape;
-                
-                //Cull the faces
-                coffeeEngine.renderer.daveshade.cullFace(DaveShade.side.FRONT);
+        editorDisplay(renderer, daveShade, camera, drawID) {
+            renderer.pipeline.setUniforms(camera, this.shader, {
+                u_model: this.mixedMatrix.webGLValue(),
+                u_colorMod: [1, 1, 1, 1],
+                u_objectID: drawID
+            })
 
-                //Set our buffers and draw
-                shader.setBuffers(coffeeEngine.shapes.cube);
-                shader.uniforms.u_model.value = this.mixedMatrix.webGLValue();
-
-                //Simple debug test
-                shader.uniforms.u_colorMod.value = [1, 1, 1, 1];
-                shader.uniforms.u_objectID.value = drawID;
-                shader.drawFromBuffers(36);
-                coffeeEngine.renderer.daveshade.cullFace();
-            }
+            daveShade.cullFace(daveShade.SIDE.FRONT);
+            this.shader.setBuffers(coffeeEngine.shapes.cube);
+            this.shader.drawFromBuffers(36);
         }
 
         getProperties() {
